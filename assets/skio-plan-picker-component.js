28d5396f210966d166bd370a76e7edee798f3b67
shomko <bogdanshomko@gmail.com>
2025-06-03 15:28:50 +0300

    theme init 1

--- assets/skio-plan-picker-component.js
+++ assets/skio-plan-picker-component.js
@@ -863,7 +863,7 @@ export class SkioPlanPicker extends LitElement {
         <ul style="width: 100%; height: 100%; padding-top: 4px; padding-inline-start: 0; flex-direction: column; justify-content: center; align-items: flex-start; gap: 20px; display: inline-flex">
           <li>
             <span>
-             ${window.customSkio.skio_one_time_text}
+              $121 OFF – 45% savings vs. buying separately
             </span>
           </li>
         </ul>
